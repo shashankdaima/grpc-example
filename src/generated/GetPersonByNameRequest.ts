a9c9ce4be0db6556758686c584e257d65be4e403
Shashank Daima <daimashashank10@gmail.com>
2024-05-21 18:37:03 +0530

    first commit

---
+++ src/generated/GetPersonByNameRequest.ts
@@ -0,0 +1,10 @@
+// Original file: proto/main.proto
+
+
+export interface GetPersonByNameRequest {
+  'name'?: (string);
+}
+
+export interface GetPersonByNameRequest__Output {
+  'name': (string);
+}
